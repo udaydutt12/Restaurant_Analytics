e92f484fe0dd09186884629d5944b134c74ab32c
Jairo Batista <jairo.batista21@gmail.com>
2020-06-07 03:15:57 -0500

    Now able to select date.

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 from os import getcwd
 
@@ -19,6 +20,23 @@ driver.get("https://www.toasttab.com/restaurants/admin/reports/home")
 sleep(1)
 driver.find_element_by_id("first-load-container").find_element_by_tag_name("button").click()
 sleep(2)
+date_dropdown = driver.find_element_by_id("date-dropdown-container")
+date_dropdown.click()
+custom_button = date_dropdown.find_element_by_link_text("Custom Date")
+custom_button.click()
+
+# Select Date for Start and End
+report_date_start = driver.find_element_by_name("reportDateStart")
+driver.execute_script("arguments[0].value = ''", report_date_start)
+report_date_start.send_keys("01-01-2020")
+report_date_start.send_keys(Keys.RETURN)
+sleep(1)
+report_date_end = driver.find_element_by_name("reportDateEnd")
+driver.execute_script("arguments[0].value = ''", report_date_end)
+report_date_end.send_keys("01-01-2020")
+report_date_end.send_keys(Keys.RETURN)
+driver.find_element_by_id("update-btn").click()
+
 summary_content = driver.find_element_by_id("sales-summary-content")
 export_button = summary_content.find_element_by_css_selector('div a')
-export_button.click()
+# export_button.click()
