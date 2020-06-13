247f72389f5bec7d7ae0f199c0aca8204bcbbcc4
udaydutt12 <udaydutt@utexas.edu>
2020-06-13 16:13:01 -0500

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-EMAIL = 'udaydutt@utexas.edu'
-PASSWORD = 'greatoaks12345'
-DRIVER_PATH = "/path/to/chrome/chromedriver"
-DB_USERNAME = 'udutt'
-DB_PASSWORD = 'greatoaks12345'
-DB_ROOT = 'restaurant_data'
